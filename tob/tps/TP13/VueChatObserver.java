bd1ba43a2ac3f9080a8e08a745469903432e6bb9
Julien Blanchon <julien@blanchon.cc>
2021-03-24 16:22:39 +0100

    Revert "commit"
    This reverts commit fee3e9738ade415ec1dff07bde6423352ec32eb5.

--- tob/tps/TP13/VueChatObserver.java
+++
@@ -1,9 +0,0 @@
-import java.util.Observable;
-import java.util.Observer;
-
-public class VueChatObserver implements Observer {
-    @Override
-    public void update(Observable o, Object arg) {
-
-    }
-}
